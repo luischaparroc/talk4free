fc648cccf69818dec8479d14ee320a02b725d7e9
Michael Sosa <833@holbertonschool.com>
2020-03-17 17:41:22 -0500

    [Feature] Languages for chats

---
+++ talk4free-frontend/src/languagesEmojis.js
@@ -0,0 +1,28 @@
+const languages = [
+  "English 🇺🇲",
+  "French 🇫🇷",
+  "Spanish 🇪🇸",
+  "Arabic 🇸🇦",
+  "Mandarin 🇨🇳",
+  "Russian 🇷🇺",
+  "Portuguese 🇵🇹",
+  "German 🇩🇪",
+  "Japanese 🇯🇵",
+  "Hindi 🇮🇳",
+  "Malay 🇲🇾",
+  "Persian 🇲🇾",
+  "Swahili 🇹🇿",
+  "Tamil 🇸🇬",
+  "Italian 🇮🇹",
+  "Dutch 🇩🇪",
+  "Bengali 🇧🇩",
+  "Turkish 🇹🇷",
+  "Vietnamese 🇻🇳",
+  "Polish 🇵🇱",
+  "Javanese 🇮🇩",
+  "Punjabi 🇵🇰",
+  "Thai 🇹🇭",
+  "Korean 🇰🇵"
+];
+
+export default languages;
