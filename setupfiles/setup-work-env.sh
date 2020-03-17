1e6047ed6fd9afb49194043680f1de5cc93808ad
luischaparroc <lfchaparro@outlook.com>
2020-03-17 16:28:15 -0500

    [Update]: moving files to back-end folder

--- setupfiles/setup-work-env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo npm install -D prettier
-sudo npm install -D eslint eslint-config-prettier
-sudo npm install -D babel-eslint eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react
-
-
